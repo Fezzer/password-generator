3108a31439f91522d610038767f340d9fe0dbfd3
Fezzer <chris.bowles@thescreamingsheep.net>
2023-01-06 14:48:31 +0000

    replace character arrays with strings to aid functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,92 +1,14 @@
-// Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+// String of special characters to be included in password
+var specialCharacters = "@%+\\/'!#$^?:,)(}{][~-_.";
 
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// String of numeric characters to be included in password
+var numericCharacters = "0123456789";
 
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+// String of lowercase characters to be included in password
+var lowerCasedCharacters = "abcdefghijklmnopqrstuvwxyz";
 
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+// String of uppercase characters to be included in password
+var upperCasedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Function to prompt user for password options
 function getPasswordOptions() {
